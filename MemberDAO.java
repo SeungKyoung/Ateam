972f0a39ce1a97f24a807e1211dc68ba90c658a9
001-10\hanul <jhcx7129@naver.com>
2021-11-16 09:28:30 +0900

    15일까지 몰빵

--- MemberDAO.java
+++ MemberDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class MemberDAO {			//DB와 연동
 	private Connection conn;		//연결 객체
@@ -69,6 +70,79 @@ public class MemberDAO {			//DB와 연동
 		return succ;
 		
 	}//memberInsert()
+
+
+	public ArrayList<MemberDTO> memberSearchAll() {
+		conn = getConn(); //db접속
+		String sql = "select * from member";
+		ArrayList<MemberDTO> list = new ArrayList<>(); //최종결과를 저장할 컬렉션 자료구조를 생성
+		try {
+			ps = conn.prepareStatement(sql);
+			rs= ps.executeQuery();
+			
+			while(rs.next()) {
+				/*String name = rs.getString("name");
+				String id = rs.getString("id");
+				String pw = rs.getString("pw");
+				int age = rs.getInt("age");
+				String addr = rs.getString("addr");
+				String tel = rs.getString("tel");
+				MemberDTO dto = new MemberDTO(name, id, pw, age, addr, tel);*/
+				MemberDTO dto = new MemberDTO();
+				dto.setName(rs.getString("name"));
+				dto.setId(rs.getString("id"));
+				dto.setPw(rs.getString("pw"));
+				dto.setAge(rs.getInt("age"));
+				dto.setAddr(rs.getString("addr"));
+				dto.setTel(rs.getString("tel"));
+				list.add(dto);
+				
+				
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("memberSearchAll() 예외오류");
+		}finally {
+			dbClose();
+		}
+		return list;
+		
+	}//memberSelect()
+
+
+	public int memberDelete(String id) {
+		conn = getConn();
+		String sql = "delete from Member where id = ?";
+		int succ = 0;
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, id);
+			succ = ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("멤버 삭제에서 예외오류");
+		}finally {
+			dbClose();
+		}
+		return succ;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
